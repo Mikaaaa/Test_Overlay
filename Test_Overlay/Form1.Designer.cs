8667c66498a7d26660eae6ca5531a753bc43a7c3
Mika <lagaffn2@live.fr>
2015-02-19 22:54:12 +0100

    test overlay youtube
    m'ettez l'adresse d'une vidéo de youtube, validez et regardez! trouvez
    lui une bonne place et elle y restera

---
+++ Test_Overlay/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace Test_Overlay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.nouvelleVidéoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.affichageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.TxtBx = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.ContextMenuStrip = this.contextMenuStrip1;
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.IsWebBrowserContextMenuEnabled = false;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(440, 322);
+            this.webBrowser1.TabIndex = 4;
+            this.webBrowser1.Visible = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nouvelleVidéoToolStripMenuItem,
+            this.affichageToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(155, 48);
+            // 
+            // nouvelleVidéoToolStripMenuItem
+            // 
+            this.nouvelleVidéoToolStripMenuItem.Name = "nouvelleVidéoToolStripMenuItem";
+            this.nouvelleVidéoToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+            this.nouvelleVidéoToolStripMenuItem.Text = "Nouvelle Vidéo";
+            this.nouvelleVidéoToolStripMenuItem.Click += new System.EventHandler(this.nouvelleVidéoToolStripMenuItem_Click);
+            // 
+            // affichageToolStripMenuItem
+            // 
+            this.affichageToolStripMenuItem.Name = "affichageToolStripMenuItem";
+            this.affichageToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+            this.affichageToolStripMenuItem.Text = "Affichage";
+            this.affichageToolStripMenuItem.Click += new System.EventHandler(this.affichageToolStripMenuItem_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 80);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(211, 133);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Modifier l\'affichage de la vidéo";
+            this.groupBox1.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(111, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Largeur";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Hauteur";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(130, 73);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Valider";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(91, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(12, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "x";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(114, 38);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(74, 20);
+            this.textBox2.TabIndex = 13;
+            this.textBox2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox2_MouseClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(7, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(74, 20);
+            this.textBox1.TabIndex = 12;
+            this.textBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseClick);
+            // 
+            // TxtBx
+            // 
+            this.TxtBx.Location = new System.Drawing.Point(12, 12);
+            this.TxtBx.Name = "TxtBx";
+            this.TxtBx.Size = new System.Drawing.Size(415, 20);
+            this.TxtBx.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(190, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Valider";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(440, 322);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.TxtBx);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.webBrowser1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "YoutubeViewer";
+            this.TopMost = true;
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem nouvelleVidéoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem affichageToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox TxtBx;
+        private System.Windows.Forms.Button button1;
+
+    }
+}
+
---
+++ Test_Overlay/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Test_Overlay
+{
+    public partial class Form1 : Form
+    {
+        String url;
+        String adr;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            url = TxtBx.Text;
+            adr = url.Substring(url.IndexOf("="), url.Length - url.IndexOf("="));
+            adr = adr.Replace("=", "");
+            groupBox1.Visible = false;
+            webBrowser1.Visible = true;
+            webBrowser1.DocumentText = "<iframe width=\"480\" height=\"360\" src=https://www.youtube.com/embed/" + adr + " frameborder=\"0\" allowfullscreen></iframe>";
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webBrowser1.Visible = false;
+            groupBox1.Visible = false;
+            textBox1.Visible = false;
+            textBox2.Visible = false;
+            label1.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+        }
+
+        private void nouvelleVidéoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            groupBox1.Visible = false;
+            TxtBx.Visible = true;
+            button1.Visible = true;
+            webBrowser1.Visible = false;
+        }
+
+        private void affichageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            groupBox1.Visible = true;
+            webBrowser1.Visible = false;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            url = TxtBx.Text;
+            adr = url.Substring(url.IndexOf("="), url.Length - url.IndexOf("="));
+            adr = adr.Replace("=", "");
+            TxtBx.Visible = false;
+            button1.Visible = false;
+            webBrowser1.Visible = true;
+            webBrowser1.DocumentText = "<iframe width=\"480\" height=\"360\" src=https://www.youtube.com/embed/" + adr + " frameborder=\"0\" allowfullscreen></iframe>";
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            TxtBx.Visible = false;
+            groupBox1.Visible = false;
+            webBrowser1.Visible = true;            
+            webBrowser1.DocumentText = "<iframe width=" + textBox1.Text + " height=\""+textBox2.Text+"\" src=https://www.youtube.com/embed/" + adr + " frameborder=\"0\" allowfullscreen></iframe>";
+            Form1.
+        }
+
+        private void textBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Double val1 = Convert.ToDouble(textBox2.Text);
+            Double resultat = val1 / 1.77;
+            textBox1.Text = Convert.ToString(Convert.ToInt32(resultat));
+        }
+
+        private void textBox2_MouseClick(object sender, MouseEventArgs e)
+        {
+            Double val1 = Convert.ToDouble(textBox1.Text);
+            Double resultat = val1 / 1.77;
+            textBox2.Text = Convert.ToString(Convert.ToInt32(resultat));
+        }
+
+    }
+}
